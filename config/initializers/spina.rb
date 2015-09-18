4df47f90322ca58669f73a0a5a3a3fe4b8d95846
Mike Peritore <michael.peritore@americastestkitchen.com>
2015-09-18 06:10:58 -0400

    initial commit

---
+++ config/initializers/spina.rb
@@ -0,0 +1,10 @@
+Spina::Engine.configure do
+  config.NEGATIVE_CAPTCHA_SECRET = '1639c8b20905f2fa8f23d80e9db9de024650209ec3aa4bf2eea3e00a0ba4f64e23a3db74651424a62ae74e0816af6f5e78a5364d373b548d6187c99c960b5067'
+
+  # If you want to use s3 to store uploads
+  # config.storage = :s3
+  # config.aws_region = "eu-west-1"
+  # config.aws_access_key_id = "abc123"
+  # config.aws_secret_key = "abc123"
+  # config.s3_bucket = "mybucket"
+end
